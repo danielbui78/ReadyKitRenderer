92ec1f03203d6055cf31ddb91f1a6c23aeb94e69
Daniel Bui <dbui2@Daniels-MacBook-Air.local>
2015-05-25 07:41:53 -0400

    Initial Commit

---
+++ ReadyKitRenderer/ReadyKitRenderer.h
@@ -0,0 +1,23 @@
+/*
+ *  ReadyKitRenderer.h
+ *  ReadyKitRenderer
+ *
+ *  Created by Daniel Bui on 5/25/15.
+ *  Copyright (c) 2015 Daniel Bui. All rights reserved.
+ *
+ */
+
+#ifndef ReadyKitRenderer_
+#define ReadyKitRenderer_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class ReadyKitRenderer
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ ReadyKitRenderer/ReadyKitRendererPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  ReadyKitRendererPriv.h
+ *  ReadyKitRenderer
+ *
+ *  Created by Daniel Bui on 5/25/15.
+ *  Copyright (c) 2015 Daniel Bui. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class ReadyKitRendererPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
